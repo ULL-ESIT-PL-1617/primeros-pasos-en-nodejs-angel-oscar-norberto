c1b0202b84d3e51118cde0dcd266e02ee6f86f14
Oscar Catari Gutierrez <alu0100825893@ull.edu.es>
2017-02-22 16:22:00 +0000

    Reconfigurando puerto

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ var path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
+app.set('port', (process.env.PORT || 8080));
+
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Manual en el puerto: ', app.get('port'));
 });
 
 /*
@@ -17,4 +23,4 @@ var server = app.listen(8080, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 })
 */
-app.listen(process.env.PORT, process.env.IP);
+//app.listen(process.env.PORT, process.env.IP);
